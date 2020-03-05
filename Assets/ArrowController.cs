461b372294cf54b401744260fc44a1dcc15eeaf9
TamerMograbi <tomispk@gmail.com>
2020-03-04 22:11:35 -0500

    arrow shows up in middle of screen
    and is alwatys pointing in the opposite direction of the camera.
    I also added a function that rotates the arrow 45 degrees when the screen is touched but it's currently not working because the arrow is updated to look at the camera on every frame and it cancels that.

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using System;
+
+public class ArrowController : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(.25f, .25f, .25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = firstPersonCamera.transform.position;
+        camPos.y -= 5f;
+        transform.LookAt(camPos);
+
+        Vector2 pos = new Vector2(Screen.width * .5f, Screen.height * .5f);
+        Ray ray = firstPersonCamera.ScreenPointToRay(pos);
+        Vector3 anchorPosition = ray.GetPoint(5f);
+
+        transform.position = Vector3.Lerp(transform.position, anchorPosition,
+              Time.smoothDeltaTime * speed);
+        ProcessTouches();
+
+    }
+
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 ||
+            (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+        //this doesn't work because lookAt(camera) is always called in update and cancels it
+        transform.Rotate(0f, 45f, 0f,Space.Self);
+    }
+}
---
+++ Assets/SceneController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuitOnConnectionError();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            // can't do anything if ARCore isn't tracking the user's movement
+            //if our status isn't tracking then we just return
+            return; 
+        }
+        // don't want screen to sleep if ARcore has enough info and is actively tracking scene
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+    
+
+    void QuitOnConnectionError()
+    {
+        if(Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit("Camera permission is required Dawg.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit("Can't connect to ARcore on device. fix yo shit",5));
+        }
+    }
+
+}
